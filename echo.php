4ab937a9a02ae257e334f7b517c2c3ebc96d3dc2
sandsoftimes <janeebee1092@gmail.com>
2024-06-09 16:59:55 +0500

    cmt

---
+++ echo.php
@@ -0,0 +1,8 @@
+<?php
+echo 10+10;
+ECHO "How are you <br/>";
+echo "ECHO is case insensitive";
+echo "<br/>";
+echo "what's up";echo "you good <br/>";
+echo("as a funtion");
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "Hello";
+?>
+<h1><?php echo "What is up"?></h1>
